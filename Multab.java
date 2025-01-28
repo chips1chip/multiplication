0f9f4b9f535f38c8cbd2c7b2d2602e0efa49de9e
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 01:50:47 +0530

    Update Multab.java

--- Multab.java
+++ Multab.java
@@ -1,12 +1,16 @@
+
 import java.util.Scanner;
 
 public class Multab {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        for (int i = 0; i < 10; i++) {
-            System.out.println(n+"*"+i+"="+n*i);
+        Scanner sc = new Scanner(System.in); // Scanner for user input
+        int num = sc.nextInt(); // Store user input number for multiplication
+
+        // Loop to print multiplication table from 1 to 9
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + num * i);
         }
-        sc.close();
+        
+        sc.close(); 
     }
 }
